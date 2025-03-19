ac0b92fdd5938bb50a5e35be4b04cf7f1abd2ded
lenovo <Mobinabakhshipour.1380@gmail.com>
2025-03-19 23:48:17 +0330

    edit NavBar & productCard & bootstrap package

--- src/components/header/componentsHeader/navBar/NavBar.js
+++ src/components/header/componentsHeader/navBar/NavBar.js
@@ -1,7 +1,6 @@
 import './NavBar.css'
 import React from 'react'
 import { BsBag, BsSearch, BsPerson } from "react-icons/bs";
-import { Form, InputGroup, ButtonToolbar } from 'react-bootstrap';
 
 export default function NavBar() {
     return (
@@ -18,8 +17,14 @@ export default function NavBar() {
                 <div className="search__container">
                     <input className="search__input" type="text" placeholder="" />
                 </div>
-                <div className='basketIcon'><a href="#"><BsBag /></a></div>
-                <div className='userIcon'><a href="#"><BsPerson /></a></div>
+                <div className='basketIcon'><a href="#"><svg width={32} height={32} fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.75 8.25A.75.75 0 0 0 3 9v10.125c0 1.418 1.207 2.625 2.625 2.625h12.75c1.418 0 2.625-1.149 2.625-2.566V9a.75.75 0 0 0-.75-.75H3.75Z" clipRule="evenodd" />
+                    <path d="M7.5 8.25v-1.5a4.5 4.5 0 0 1 4.5-4.5v0a4.5 4.5 0 0 1 4.5 4.5v1.5" />
+                </svg></a></div>
+                <div className='userIcon'><a href="#"><svg width={32} height={32} fill="none" stroke="#000000" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16.125 6.75c-.184 2.478-2.063 4.5-4.125 4.5-2.063 0-3.945-2.021-4.125-4.5-.188-2.578 1.64-4.5 4.125-4.5 2.484 0 4.312 1.969 4.125 4.5Z" />
+                    <path d="M12 14.25c-4.078 0-8.217 2.25-8.983 6.497-.092.512.197 1.003.733 1.003h16.5c.536 0 .826-.491.734-1.003C20.217 16.5 16.078 14.25 12 14.25Z" />
+                </svg></a></div>
 
             </div>
         </div>
